8f6ddb3fb05efdb91f49325aaa4d67bd7d636028
Charley <charleylla@foxmail.com>
2017-12-04 11:41:17 +0800

    增加静态资源目录的相关注释说明

--- nodeapp/build/config/index.js
+++ nodeapp/build/config/index.js
@@ -19,8 +19,19 @@ CONFIG.set("PORT", 3000);
 CONFIG.set("VIEWS_DIR", _path2.default.resolve(__dirname, "../views"));
 // 定义静态资源路径
 // 原始路径
-// CONFIG.set("PUBLIC_DIR",path.resolve(__dirname,"../public"));
-CONFIG.set("PUBLIC_DIR", _path2.default.resolve(__dirname, "../"));
-console.log(_path2.default.resolve(__dirname, "../"));
+CONFIG.set("PUBLIC_DIR", _path2.default.resolve(__dirname, "../public"));
+// CONFIG.set("PUBLIC_DIR",path.resolve(__dirname,"../"));
+/**
+ * 为什么这里需要修改路径呢？
+ * 这是因为我们在使用 webpack 进行编译的时候，将静态资源的目录设置为 build：
+ *      path: path.resolve(__dirname, "../build")
+ * 生成的 js 和 css 文件路径如下：
+ *      filename: "public/js/[name]-[hash:16].js"
+ *      public/css/main-[hash:16].css
+ * 这些生成的文件都是以 build 为基准，存放在 public 目录中的，而自动生成的 HTML 文件是这样插入 js 的：
+ *      <script type='text/javascript' src='../public/js/tags-8eb2e455ac139462.js'></script>
+ * 因此，只需要将
+ * 
+ */
 
 exports.default = CONFIG;
